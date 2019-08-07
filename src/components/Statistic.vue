9463ca42f4ca338f058f96183be65e136a217b8f
Freddy Heppell <freddy@freddyheppell.com>
2019-08-07 12:47:41 +0100

    Center text in row

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -1,12 +1,10 @@
 <template>
     <div v-if="show" style="height: 200px;">
-        <div>
             <div class="whitebg w-full"></div>
             <div class="blackbg" :style="'width:' + area1Percent + '%'"></div>
             <div class="makered w-full" ></div>
 
             <span class="crime-name font-bold">{{ name }}</span>
-        </div>
     </div>
 </template>
 
