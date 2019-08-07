a8f5c7c978925ad944305707e58ed4dfee533e22
Freddy Heppell <freddy@freddyheppell.com>
2019-08-07 15:22:13 +0100

    add uncss for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,20 @@
-const tailwindcss = require("tailwindcss");
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue'
+  ]
+})
+
+
 
 module.exports = {
   plugins: [
-    // ...
     require('tailwindcss'),
     require('autoprefixer'),
-    // ...
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
