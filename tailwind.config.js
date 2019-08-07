fedec4e2a54c296cfa1964ecb75d9075a260feee
Freddy Heppell <freddy@freddyheppell.com>
2019-08-07 15:33:46 +0100

    Reduce colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        "police-blue": "#1b4080"
-      }
+    colors: {
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      blue: colors.blue,
+      yellow: colors.yellow,
+      green: colors.green,
+      "police-blue": "#1b4080"
     },
     fontFamily: {
       "sans": ["Helvetica Neue", "Helvetica", "sans-serif"]
