f280963498c6591891828864a0dbb97f31d87a2c
Freddy Heppell <freddy@freddyheppell.com>
2019-08-07 15:25:06 +0100

    Remove uncss for now

--- postcss.config.js
+++ postcss.config.js
@@ -12,9 +12,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
+    require('autoprefixer')
   ]
 }
