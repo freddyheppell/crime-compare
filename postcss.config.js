e1d20b984a037cbc7c3a0a7ef9edc28060274c5e
Freddy Heppell <freddy@freddyheppell.com>
2019-08-07 15:26:26 +0100

    properly remove uncss

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-  // Specify the paths to all of the template files in your project 
-  content: [
-    './src/**/*.html',
-    './src/**/*.vue'
-  ]
-})
-
-
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
